fa5a5afcbb65cc5d626ca035f3b9daca22bd6a54
Nicole Childs <n.c.childs@gmail.com>
2018-10-15 11:13:57 -0500

    Version 5

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 var todoList = { 
-  todos: ["item 1", "item 2", "item 3"],
+  todos: [],
   displayTodos: function (){
-    console.log("My Todos", this.todos)
+    if (this.todos.length === 0){
+      console.log("Your todo list is empty.");
+    } else {
+      console.log("My Todos: ");
+      for (var i = 0; i < this.todos.length; i++ ){
+        if (this.todos[i].completed === true) {
+          console.log('(x)', this.todos[i].todoText);
+        } else {
+          console.log('()', this.todos[i].todoText);
+        }
+      }
+    }
   },
   addTodo: function(todoText){
     this.todos.push({
@@ -25,7 +36,3 @@ var todoList = {
   }
 };
 
-addTodo("some stufff");
-changeTodo(0, "buy catfood")
-deleteTodos(1);
-displayTodos();
