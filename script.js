9e34f0efe42df4c70247fc2a870ba5a008015726
Nicole Childs <n.c.childs@gmail.com>
2018-10-15 11:32:28 -0500

    Version 6

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var todoList = { 
   todos: [],
-  displayTodos: function (){
-    if (this.todos.length === 0){
+  displayTodos: function () {
+    if (this.todos.length === 0) {
       console.log("Your todo list is empty.");
     } else {
       console.log("My Todos: ");
-      for (var i = 0; i < this.todos.length; i++ ){
+      for (var i = 0; i < this.todos.length; i++ ) {
         if (this.todos[i].completed === true) {
           console.log('(x)', this.todos[i].todoText);
         } else {
@@ -14,7 +14,7 @@ var todoList = {
       }
     }
   },
-  addTodo: function(todoText){
+  addTodo: function(todoText) {
     this.todos.push({
       todoText: todoText, // 2nd todoText is parameter
       completed: false 
@@ -25,13 +25,36 @@ var todoList = {
     this.todos[position].todoText = todoText;
     this.displayTodos();
   },
-  deleteTodos: function(position){
+  deleteTodos: function(position) {
     this.todos.splice(position, 1);
     this.displayTodos();
   },
-  toggleCompleted: function(position){
+  toggleCompleted: function(position) {
     var todo = this.todos[position];
     todo.completed = !todo.completed;
+    this.displayTodos();
+  },
+  toggleAll: function() {
+    var totalTodos = this.todos.length;
+    var completedTodos = 0;
+
+    for (var i = 0; i < totalTodos; i++) {
+      if (this.todos[i].completed === true) {
+        completedTodos++;
+      }
+    }
+    //Case 1:  if everything is true, make everything false
+    if (completedTodos === totalTodos) {
+      for (var i = 0; i < totalTodos; i++) {
+         this.todos[i].completed = false;
+      }
+      //Case2:  otherwise make everything true
+    } else {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = true;
+      }
+    }
+
     this.displayTodos();
   }
 };
