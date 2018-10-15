7888c81b48c4e3d31417e5771d7f17ea9765a402
Nicole Childs <n.c.childs@gmail.com>
2018-10-15 09:37:39 -0500

    Version 2

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 var todos = ["item 1", "item 2", "item 3"];
-console.log("My Todos:", todos);
-todos.push("item 5");
-todos[0] = "item 1 updated";
-todos.splice(0, 1);
+function displayTodos (){
+  console.log("My Todos:", todos);
+};
+
+function addTodo(todo){
+  todos.push(todo);
+  displayTodos();
+};
+
+function changeTodo(position, newValue){
+  todos[position] = newValue; 
+  displayTodos();
+};
+
+function deleteTodos(position){
+  todos.splice(position, 1);
+  displayTodos();
+}
+
+addTodo("some stufff");
+changeTodo(0, "buy catfood")
+deleteTodos(1);
+displayTodos();
